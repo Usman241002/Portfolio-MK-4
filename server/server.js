15f893f181f03152b76fea1d6cafb9544787fe35
Usman Khalid <ukhalid428@gmail.com>
2026-07-08 20:36:05 +0100

    Prepare backend docker deployment

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const PORT = 3000; //server listening on port 3000
 //load up schema
 await initDB();
 
-//enabling the server to listen on port 3000
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+//enabling the server to listen on port
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on ${PORT}`)
+})
